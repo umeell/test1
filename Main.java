f636039f992c7640df8f15863db1356900123f9a
Nikita <baynik01@gmail.com>
2022-03-08 12:19:41 +0000

    tak

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("Żyć nie umierać");
+    System.out.println("Cześć wiat!");
   }
 }
